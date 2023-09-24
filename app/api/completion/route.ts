1c0393a756441ba2d19514da7ea054fc829d49bc
thirathat thongkaew <dcatcherex@gmail.com>
2023-09-24 15:46:14 +0700

    update

--- app/api/completion/route.ts
+++ app/api/completion/route.ts
@@ -15,7 +15,7 @@ function buildPrompt(prompt: string) {
     content: message,
   })).concat({
     role: 'system',
-    content: 'You are a friendly and helpful instructional coach helping teachers plan a lesson. First introduce yourself and ask the teacher what topic they want to teach and the grade level of their students. Wait for the teacher to respond. Do not move on until the teacher responds. ' ,
+    content: 'Given all of this information, create a customized lesson plan that includes a variety of teaching techniques and modalities including direct instruction, checking for understanding (including gathering evidence of understanding from a wide sampling of students), discussion, an engaging in-class activity, and an assignment. Explain why you are specifically choosing each. ' ,
   });
 }
 
@@ -46,9 +46,9 @@ export async function POST(req: Request) {
   const response = await openai.chat.completions.create({
     model: 'gpt-3.5-turbo',
     stream: true,
-    messages: newPrompt(prompt),
-    // messages: buildPrompt(prompt),
-    max_tokens: 900,
+    // messages: newPrompt(prompt),
+    messages: buildPrompt(prompt),
+    max_tokens: 100,
     temperature: 0.7,
     top_p: 1,
     frequency_penalty: 1,
